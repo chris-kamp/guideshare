e39545edfa1b1b1c3f205d766d69b66a0f662cf8
Chris Kamp <chriskamp101@gmail.com>
2021-05-14 11:09:52 +1000

    implement editing guides

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -10,8 +10,11 @@ class GuidesController < ApplicationController
   end
 
   def create
+    @guide = Guide.new(guide_params)
+    @guide.user = current_user
     if @guide.save
-      redirect_to @guide, notice: "Guide \"#{@guide.title}\" was successfully created."
+      redirect_to @guide,
+                  notice: "Guide \"#{@guide.title}\" was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -19,7 +22,14 @@ class GuidesController < ApplicationController
 
   def edit; end
 
-  def update; end
+  def update
+    if @guide.update(guide_params)
+      redirect_to @guide,
+                  notice: "Guide \"#{@guide.title}\" was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   def destroy; end
 
@@ -27,5 +37,9 @@ class GuidesController < ApplicationController
 
   def set_guide
     @guide = Guide.find(params[:id])
+  end
+
+  def guide_params
+    params.require(:guide).permit(:title, :description)
   end
 end
