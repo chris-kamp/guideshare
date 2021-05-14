eb2a6c6a0c0322bc3c072b887ba457d4646a5f37
Chris Kamp <chriskamp101@gmail.com>
2021-05-14 11:50:03 +1000

    implement guide show page

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -2,8 +2,8 @@ class Guide < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
 
-  # Returns true if the guide has a description that is not empty or comprised only of whitespace
-  def has_description?
-    return description && !description.strip.empty?
+  # Returns the guide's description if not empty, or else "no description provided"
+  def description_text
+    return description && !description.strip.empty? ? description : "No description provided"
   end
 end
