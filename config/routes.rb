f40f839fdc34d7d9bbaf6ca577b5b980a1c5d90d
Chris Kamp <chriskamp101@gmail.com>
2021-05-15 20:50:39 +1000

    add links to guide view page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   resources :guides
-  get "/guides/:id/view", to: "guides#view"
+  get "/guides/:id/view", to: "guides#view", as: "view_guide"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
