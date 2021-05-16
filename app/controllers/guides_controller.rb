51731fa70dc4e827522f359d15a2dc44a7b7ee17
Chris Kamp <chriskamp101@gmail.com>
2021-05-16 21:06:10 +1000

    add guide to user account following successful checkout

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,28 +1,33 @@
 class GuidesController < ApplicationController
   before_action :authenticate_user!,
                 only: %i[new create edit update destroy view purchase owned]
-  before_action :set_guide, only: %i[show edit update destroy view purchase buy]
-  skip_before_action :verify_authenticity_token, only: [:buy]
+  before_action :set_guide, only: %i[show edit update destroy view purchase success cancel]
+  skip_before_action :verify_authenticity_token, only: [:purchase]
 
+  # GET /guides
   def index
     # Retrieve all guides for display
     @guides = Guide.all
   end
 
+  # GET /guides/:id
   # Show the details of a guide (but not the file containing guide contents)
   def show; end
 
+  # GET/guides/:id/view
   # View the content of a guide
   def view
-    # Get number of pages in guide file for display
+    # Get number of pages in guide file for display. Makes a Cloudinary API call.
     @page_count = @guide.get_page_count
   end
 
+  # GET /guides/new
   def new
     # Create an empty guide object to extract attributes for new guide form
     @guide = Guide.new
   end
 
+  # POST /guides
   def create
     # Create a new guide using strong params from form data, assigned to current user
     @guide = current_user.guides.new(guide_params)
@@ -36,8 +41,10 @@ class GuidesController < ApplicationController
     end
   end
 
+  # GET /guides/:id/edit
   def edit; end
 
+  # PUT/PATCH /guides/:id
   def update
     # Update a guide using strong params from form data.
     # # Redirect and notify if successful, or re-render "edit" page with error messages if update fails.
@@ -49,55 +56,58 @@ class GuidesController < ApplicationController
     end
   end
 
+  # DELETE /guides/:id
   def destroy
     # Delete guide and redirect to index with success notice
     @guide.destroy
     redirect_to guides_url, notice: 'Guide was successfully destroyed.'
   end
 
-  # Add a guide to the current user's owned guides
+  # POST /guides/:id/purchase
   def purchase
-    # Notify failure if user already owns the guide. Otherwise, add to owned guides and notify success.
+    # Prevent initiating purchase if guide already owned
     if @guide.owned_by?(current_user)
-      redirect_to @guide, alert: "Purchase failed: you already own this guide."
+      redirect_to @guide, alert: "Unable to purchase: you already own this guide."
+    # Otherwise, create Stripe session and render checkout
     else
-      current_user.owned_guides.push(@guide)
-      redirect_to @guide, notice: "Guide \"#{@guide.title}\" was successfully purchased."
+      Stripe.api_key = ENV['STRIPE_API_KEY']
+      session = Stripe::Checkout::Session.create({
+        payment_method_types: ['card'],
+        line_items: [{
+          price_data: {
+            currency: 'aud',
+            product_data: {
+              name: @guide.title,
+            },
+            unit_amount: 500,
+          },
+          quantity: 1,
+        }],
+        mode: 'payment',
+        # These routes handle successful or canceled checkout
+        success_url: "#{request.base_url}/guides/#{@guide.id}/purchase-success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: "#{request.base_url}/guides/#{@guide.id}/purchase-cancel",
+      })
+      render json: session
     end
   end
 
-  def buy
-    Stripe.api_key = ENV['STRIPE_API_KEY']
-    session = Stripe::Checkout::Session.create({
-      payment_method_types: ['card'],
-      line_items: [{
-        price_data: {
-          currency: 'aud',
-          product_data: {
-            name: @guide.title,
-          },
-          unit_amount: 500,
-        },
-        quantity: 1,
-      }],
-      mode: 'payment',
-      # These placeholder URLs will be replaced in a following step.
-      success_url: request.base_url + '/guides/purchase-success',
-      cancel_url: request.base_url + '/guides/purchase-cancel',
-    })
-
-    render json: session
-  end
-
+  # GET /guides/:id/purchase-success
+  # Called when Stripe checkout succeeds
   def success
-    render plain: "Successful purchase"
-
+    # Add purchased guide to owned guides, unless already present
+    current_user.owned_guides.push(@guide) unless @guide.owned_by?(current_user)
+    redirect_to @guide, notice: "Thank you for your purchase!"
   end
 
+  # GET /guides/:id/purchase-cancel
+  # Called when Stripe checkout canceled
   def cancel
-    render plain: "Canceled transaction"
+    redirect_to @guide, alert: "Transaction canceled."
   end
 
+  # GET /guides/owned
+  # Display all guides owned (purchased) by the current user
   def owned
     @guides = current_user.owned_guides
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-
   # Place /guides/x routes above resources to override "/guides/:id" show path
   get "/guides/owned", to: "guides#owned", as: "owned_guides"
-  get "/guides/purchase-success", to: "guides#success"
-  get "/guides/purchase-cancel", to: "guides#cancel"
   resources :guides
-  post "/guides/:id/buy", to: "guides#buy"
+  post "/guides/:id/purchase", to: "guides#purchase"
+  get "/guides/:id/purchase-success", to: "guides#success"
+  get "/guides/:id/purchase-cancel", to: "guides#cancel"
   get "/guides/:id/view", to: "guides#view", as: "view_guide"
-  get "/guides/:id/purchase", to: "guides#purchase", as: "purchase_guide"
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
