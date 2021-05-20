4b2578ca0e87e71b55e6f2f5020db82f56d70cfb
Chris Kamp <chriskamp101@gmail.com>
2021-05-20 13:57:19 +1000

    add carts controller and show page

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,16 @@
+class CartsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_cart
+
+  def show
+    @guides = @cart.guides
+  end
+
+  private
+
+  # Because users can only view their own cart, retrieve the current user's cart
+  # (noting that user login is authenticated occurs before this step)
+  def set_cart
+    @cart = current_user.cart
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  # ROOT
   # Root path routes to home page
   root to: "pages#home"
 
+  # GUIDES
   # Place /guides/x routes above resources to override "/guides/:id" show path
   get "/guides/owned", to: "guides#owned", as: "owned_guides"
   get "/guides/dashboard", to: "guides#dashboard", as: "guides_dashboard"
 
   # Use shallow nesting to include :guide_id param in index, create and new Review paths
   resources :guides, shallow: true do
+    # REVIEWS
     resources :reviews, except: :show
   end
 
@@ -19,6 +22,11 @@ Rails.application.routes.draw do
   get "/guides/:id/archive", to: "guides#archive", as: "archive_guide"
   get "/guides/:id/restore", to: "guides#restore", as: "restore_guide"
 
+  # CARTS
+  # No restful params required as users can only view their own cart
+  get "/cart", to: "carts#show", as: "cart"
+
+  # USERS
   # Use custom SignupsController to handle registrations (extends devise default to provide added functionality)
   devise_for :users, controllers: { registrations: "signups" }
 end
