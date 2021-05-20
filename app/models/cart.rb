ad5f4426fb1d6983504f1ceece7ab701a03c6528
Chris Kamp <chriskamp101@gmail.com>
2021-05-20 16:53:17 +1000

    add total to cart display

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,8 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_guides, dependent: :destroy
   has_many :guides, through: :cart_guides
+
+  def total
+    return guides.sum(&:price)
+  end
 end
