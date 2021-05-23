9373260d2bb6de32bda0a7180305327d9fbfd39c
Chris Kamp <chriskamp101@gmail.com>
2021-05-23 15:49:36 +1000

    add further tags to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,7 +52,28 @@ tags = Tag.create([
   },
   {
     name: "Finance"
-  }
+  },
+  {
+    name: "Cooking"
+  },
+  {
+    name: "Productivity"
+  },
+  {
+    name: "Short"
+  },
+  {
+    name: "Long"
+  },
+  {
+    name: "Beginner"
+  },
+  {
+    name: "Intermediate"
+  },
+  {
+    name: "Expert"
+  },
 ])
 
 # To avoid excessive Cloudinary requests during testing, load one attachment and replicate for other seeded guides.
@@ -69,7 +90,8 @@ guide_template =
       io: guide_attachment,
       filename: 'lorem.pdf',
     },
-    price: 1.5
+    price: 1.5,
+    tags: [tags[0], tags[3]]
   )
 
 # Seed guides for each user
@@ -78,14 +100,15 @@ user.guides.create(
     {
       title: 'Guide with no description',
       guide_file: guide_template.guide_file.blob,
-      tags: [tags[0], tags[3]]
+      tags: [tags[0], tags[3]],
+      price: 10.5
     },
     {
       title: 'Guide 3',
       description: 'This is the description',
       guide_file: guide_template.guide_file.blob,
       price: 0,
-      tags: [tags[1]]
+      tags: [tags[1], tags[10]]
     },
   ],
 )
@@ -96,13 +119,14 @@ admin.guides.create(
       description: "This one's by the admin",
       guide_file: guide_template.guide_file.blob,
       price: 5.5,
-      tags: [tags[0], tags[4]]
+      tags: [tags[0], tags[4], tags[7]]
     },
     {
       title: 'Another admin guide with a long description',
       description:
         'This is a long description to see what that looks like. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus.',
       guide_file: guide_template.guide_file.blob,
+      price: 1
     },
   ],
 )
@@ -112,18 +136,22 @@ devtester.guides.create(
       title: 'Definitely a guide',
       description: "Pretty sure it's a guide",
       guide_file: guide_template.guide_file.blob,
-      price: 25
+      price: 25,
+      tags: [tags[7], tags[10], tags[2]]
     },
     {
       title:
         "This is a guide with a very long title that is the max length 120 characters long, almost, but doesn't quite get there",
       description: 'This is a short description of a guide with a long title',
       guide_file: guide_template.guide_file.blob,
-      price: 9.95
+      price: 9.95,
+      tags: [tags[6], tags[7]]
     },
     {
       title: 'Running out of titles',
       guide_file: guide_template.guide_file.blob,
+      tags: [tags[9], tags[5]],
+      price: 4.5
     },
   ],
 )
