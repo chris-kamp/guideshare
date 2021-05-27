bb7bee26575ef7790ad51db4d6e5879d58823f6d
Chris Kamp <chriskamp101@gmail.com>
2021-05-27 19:03:01 +1000

    expand seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,211 +1,216 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-require 'uri'
-
 User.destroy_all
 Guide.destroy_all
 Tag.destroy_all
 
 # Seed users
-user =
+user1 =
   User.create(
-    email: 'user@user.com',
-    username: 'user',
-    password: ENV['SEED_USER_PASSWORD'],
+    email: 'user1@user1.com',
+    username: 'user1',
+    password: "password",
     cart: Cart.new
   )
-admin =
+devguides =
   User.create(
-    email: 'admin@admin.com',
-    username: 'admin',
-    password: ENV['SEED_ADMIN_PASSWORD'],
+    email: 'devguides@devguides.com',
+    username: 'devguides',
+    password: "password",
     cart: Cart.new
   )
-devtester =
+workouts =
   User.create(
-    email: 'devtester9001@gmail.com',
-    username: 'devtester',
-    password: ENV['SEED_DEVTESTER_PASSWORD'],
+    email: 'bestworkouts@bestworkouts.com',
+    username: 'BestWorkouts',
+    password: "password",
+    cart: Cart.new
+  )
+budgeting =
+  User.create(
+    email: 'budgeting@budgeting.com',
+    username: 'personal_finance_tips42',
+    password: "password",
+    cart: Cart.new
+  )
+reader =
+  User.create(
+    email: 'reader@reader.com',
+    username: 'reader',
+    password: "password",
     cart: Cart.new
   )
 anon1 =
   User.create(
     email: 'anon1@anon1.com',
-    username: 'anon1',
-    password: "anon1aaa",
+    username: 'anonymous1',
+    password: "password",
     cart: Cart.new
   )
 anon2 =
   User.create(
     email: 'anon2@anon2.com',
-    username: 'anon2',
-    password: "anon1aaa",
+    username: 'anonymous2',
+    password: "password",
     cart: Cart.new
   )
 anon3 =
   User.create(
     email: 'anon3@anon3.com',
-    username: 'anon3',
-    password: "anon1aaa",
+    username: 'anonymous3',
+    password: "password",
     cart: Cart.new
   )
 anon4 =
   User.create(
     email: 'anon4@anon4.com',
-    username: 'anon4',
-    password: "anon1aaa",
+    username: 'anonymous4',
+    password: "password",
     cart: Cart.new
   )
 anon5 =
   User.create(
     email: 'anon5@anon5.com',
-    username: 'anon5',
-    password: "anon1aaa",
-    cart: Cart.new
-  )
-anon6 =
-  User.create(
-    email: 'anon6@anon6.com',
-    username: 'anon6',
-    password: "anon1aaa",
-    cart: Cart.new
-  )
-anon7 =
-  User.create(
-    email: 'anon7@anon7.com',
-    username: 'anon7',
-    password: "anon1aaa",
+    username: 'anonymous5',
+    password: "password",
     cart: Cart.new
   )
 
 # Seed tags
 
-tags = Tag.create([
-  {
-    name: "DIY"
-  },
-  {
-    name: "Tech"
-  },
-  {
-    name: "Health & Fitness"
-  },
-  {
-    name: "Arts & Crafts"
-  },
-  {
-    name: "Finance"
-  },
-  {
-    name: "Cooking"
-  },
-  {
-    name: "Productivity"
-  },
-  {
-    name: "Short"
-  },
-  {
-    name: "Long"
-  },
-  {
-    name: "Beginner"
-  },
-  {
-    name: "Intermediate"
-  },
-  {
-    name: "Expert"
-  },
-])
+tags = {
+  diy: Tag.create(name: "DIY"),
+  tech: Tag.create(name: "Tech"),
+  dev: Tag.create(name: "Software Development"),
+  fitness: Tag.create(name: "Health & Fitness"),
+  art: Tag.create(name: "Arts & Crafts"),
+  finance: Tag.create(name: "Personal Finance"),
+  cooking: Tag.create(name: "Cooking"),
+  productivity: Tag.create(name: "Productivity"),
+  short: Tag.create(name: "Short"),
+  long: Tag.create(name: "Long"),
+  beginner: Tag.create(name: "Beginner"),
+  intermediate: Tag.create(name: "Intermediate"),
+  expert: Tag.create(name: "Expert")
+}
+
 
 # To avoid excessive Cloudinary requests, when seeding, load one attachment and replicate for other seeded guides.
-guide_attachment = File.open(File.join(Rails.root, 'app/assets/documents/lorem.pdf'))
+guide_attachment1 = File.open(File.join(Rails.root, 'app/assets/documents/lorem.pdf'))
+guide_attachment2 = File.open(File.join(Rails.root, 'app/assets/documents/two-page-lorem.pdf'))
 
-guide_template =
-  user.guides.create(
-    title: "User's first guide",
-    description: 'The very first guide',
+guide_template1 =
+  user1.guides.create(
+    title: "How to create a guide",
+    description: 'The most important guide of all',
     guide_file: {
-      io: guide_attachment,
+      io: guide_attachment1,
       filename: 'lorem.pdf',
     },
-    price: 1.5,
-    tags: [tags[0], tags[3]]
+    tags: [tags[:tech], tags[:short], tags[:beginner]]
   )
 
-# Seed guides for each user
-user.guides.create(
+guide_template2 =
+  user1.guides.create(
+    title: "GuideShare Tips and Tricks",
+    description: 'Great ways to get more out of GuideShare',
+    guide_file: {
+      io: guide_attachment2,
+      filename: 'two-page-lorem.pdf',
+    },
+    tags: [tags[:tech], tags[:long], tags[:intermediate]]
+  )
+
+lorem = user1.guides.create(
+    title: 'What does Lorem Ipsum really mean?',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus.',
+    guide_file: guide_template1.guide_file.blob,
+    price: 1,
+    reviews: [
+      Review.create(user: anon1, rating: 5, content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus."),
+      Review.create(user: anon2, rating: 5, content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus."),
+      Review.create(user: anon3, rating: 5, content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus."),
+      Review.create(user: anon4, rating: 5, content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus."),
+      Review.create(user: anon5, rating: 5, content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus."),
+      Review.create(user: reader, rating: 0, content: "I don't get it"),
+    ]
+)
+
+[anon1, anon2, anon3, anon4, anon5, reader].each { |user| user.owned_guides.push(lorem) }
+
+workouts.guides.create(
   [
     {
-      title: 'Guide with no description',
-      guide_file: guide_template.guide_file.blob,
-      tags: [tags[0], tags[3]],
-      price: 10.5
+      title: 'Top 10 quarantine workouts',
+      description: "Stay active while staying home",
+      guide_file: guide_template1.guide_file.blob,
+      price: 4.95,
+      tags: [tags[:fitness], tags[:productivity]]
     },
     {
-      title: 'Guide 3',
-      description: 'This is the description',
-      guide_file: guide_template.guide_file.blob,
-      price: 0,
-      tags: [tags[1], tags[10]]
+      title: 'How to create a diet plan',
+      guide_file: guide_template2.guide_file.blob,
+      price: 4.95,
+      tags: [tags[:fitness], tags[:cooking]]
     },
   ],
 )
-admin.guides.create(
+budgeting.guides.create(
   [
     {
-      title: 'Another guide',
-      description: "This one's by the admin",
-      guide_file: guide_template.guide_file.blob,
-      price: 5.5,
-      tags: [tags[0], tags[4], tags[7]]
-    },
-    {
-      title: 'Another admin guide with a long description',
-      description:
-        'This is a long description to see what that looks like. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus, error odit necessitatibus magnam itaque rem voluptates fugit nesciunt? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quam assumenda temporibus possimus repudiandae fuga corrupti corporis quod non voluptatibus.',
-      guide_file: guide_template.guide_file.blob,
-      price: 1
-    },
-  ],
-)
-devtester.guides.create(
-  [
-    {
-      title: 'Definitely a guide',
-      description: "Pretty sure it's a guide",
-      guide_file: guide_template.guide_file.blob,
-      price: 25,
-      tags: [tags[7], tags[10], tags[2]]
-    },
-    {
-      title:
-        "This is a guide with a very long title that is the max length 120 characters long, almost, but doesn't quite get there",
-      description: 'This is a short description of a guide with a long title',
-      guide_file: guide_template.guide_file.blob,
+      title: "Your first budgeting spreadsheet",
+      guide_file: guide_template1.guide_file.blob,
       price: 9.95,
-      tags: [tags[6], tags[7]]
+      tags: [tags[:finance], tags[:long]]
     },
     {
-      title: 'Running out of titles',
-      guide_file: guide_template.guide_file.blob,
-      tags: [tags[9], tags[5]],
-      price: 4.5
+      title: 'Personal finance basics',
+      description: "By following this one simple guide, you too can become a millionaire, probably",
+      guide_file: guide_template2.guide_file.blob,
+      price: 99.95,
+      tags: [tags[:beginner], tags[:short], tags[:finance]],
+      reviews: [
+        Review.create(user: reader, rating: 3, content: "A bit overpriced"),
+        Review.create(user: anon1, rating: 2)
+      ]
     },
   ],
 )
 
-devtester.guides.first.reviews.create(content: "Anon 1's review", rating: 5, user: anon1)
-devtester.guides.first.reviews.create(content: "Anon 2's review", rating: 2, user: anon2)
-devtester.guides.first.reviews.create(content: "Anon 3's review", rating: 3, user: anon3)
-devtester.guides.first.reviews.create(content: "Anon 4's review", rating: 4, user: anon4)
-devtester.guides.first.reviews.create(content: "Anon 5's review", rating: 5, user: anon5)
-devtester.guides.first.reviews.create(content: "Anon 6's review", rating: 0, user: anon6)
-devtester.guides.first.reviews.create(content: "Anon 7's review", rating: 2, user: anon7)
+reader.owned_guides.push(budgeting.guides.last)
+anon1.owned_guides.push(budgeting.guides.last)
+
+devguides.guides.create(
+  [
+    {
+      title: 'Understanding prototypal inheritance in JavaScript',
+      description: "It's a bit complicated",
+      guide_file: guide_template2.guide_file.blob,
+      price: 10,
+      tags: [tags[:tech], tags[:dev], tags[:expert]],
+      reviews: [
+        Review.create(user: reader, rating: 1, content: "I'm still confused")
+      ]
+    },
+    {
+      title: 'How to build your first Ruby on Rails app',
+      description: "Get started now with one simple command",
+      guide_file: guide_template1.guide_file.blob,
+      tags: [tags[:tech], tags[:dev], tags[:beginner]],
+      price: 2
+    },
+    {
+      title: 'Top 10 VSCode extensions every Rails dev needs',
+      guide_file: guide_template2.guide_file.blob,
+      price: 5,
+      tags: [tags[:tech], tags[:dev], tags[:intermediate]],
+      reviews: [
+        Review.create(user: reader, rating: 3, content: "Pretty helpful"),
+        Review.create(user: anon1, rating: 4),
+        Review.create(user: anon2, rating: 1),
+      ]
+    },
+  ],
+)
+
+reader.owned_guides.push(devguides.guides.first)
+[reader, anon1, anon2].each { |user| user.owned_guides.push(devguides.guides.last) }
